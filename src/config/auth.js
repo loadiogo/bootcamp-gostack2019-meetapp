fd2ff51f0a57bb2e108dab8cff4a18aa5088f139
loadiogo <loadiogo@DESKTOP-0VAM97U.localdomain>
2019-07-08 19:19:25 +0000

    added new middleware and session controller

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f8fc316701f15a848997ec089b53ac5f',
+  expiresIn: '7d',
+};
---
+++ src/server.js
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(3334, () => {
+  console.log('Listening on port 3334');
+});
