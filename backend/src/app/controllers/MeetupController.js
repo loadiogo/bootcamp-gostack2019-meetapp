79e58dc388e96f3ea6e5f044b746cd8084fd493b
loadiogo <loadiogo@DESKTOP-0VAM97U.localdomain>
2019-07-22 02:20:17 +0000

    Added new meetup validation
    > All properties are covered
    
    > User cannot create with past dates

--- backend/src/app/controllers/MeetupController.js
+++ backend/src/app/controllers/MeetupController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { isBefore, parseISO } from 'date-fns';
 
 import Meetup from '../models/Meetup';
 
@@ -9,15 +10,39 @@ class MeetupController {
       description: Yup.string().required(),
       location: Yup.string().required(),
       date: Yup.date().required(),
+      banner_id: Yup.number().required(),
+      user_id: Yup.number().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
+    const formatedDate = parseISO(req.body.date);
+
+    if (isBefore(formatedDate, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
     const meetup = await Meetup.create(req.body);
 
     return res.json(meetup);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+      banner_id: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    return res.json({});
   }
 }
 export default new MeetupController();
