03a3c259e65be7faebd52e61adc9bc9bf0507c87
loadiogo <loadiogo@DESKTOP-0VAM97U.localdomain>
2019-07-24 02:18:16 +0000

    Added new subscription system for meetups

---
+++ backend/src/app/controllers/SubscriptionController.js
@@ -0,0 +1,13 @@
+import Subscription from '../models/Subscription';
+
+class SubscriptionController {
+  async index(req, res) {
+    return res.json({});
+  }
+
+  async create(req, res) {
+    return res.json({});
+  }
+}
+
+export default new SubscriptionController();
